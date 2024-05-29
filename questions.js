6caf83371bf9d17f4d5edea505a09ac6ae7c69ca
Alberto Alvarez <albertoalvarez@vinixcode.com>
2024-05-29 14:00:28 -0500

    Agregando preguntas y colocando en archivo json

---
+++ questions.js
@@ -0,0 +1,334 @@
+export const questions = {
+  DROOP: {
+    questions: [
+      {
+        question:
+          '¿En qué momento se realiza la AUTORIZACIÓN DE LA CARGA DE TRABAJO (única respuesta)?',
+        options: [
+          'Al momento de realizar la recogida',
+          'Desde el Check point Descargue ruta Nacional',
+          'Al momento de FINALIZAR el checkpoint Asignación',
+          'En la calle cuando estamos entregando',
+        ],
+        correct: 2,
+      },
+      {
+        question:
+          '¿Porque opción de la TIM realizarías efectivas estas recogidas? UXU?',
+        options: [
+          'Opción 7 Recogida efectiva',
+          'Opción 10 Recogida AGW',
+          'Opción 11 Recogida UXU',
+          'Opción 5 Liquidar Guía]',
+        ],
+        correct: 2,
+      },
+      {
+        question: '¿Que es una recogida FC?',
+        options: [
+          'Recogida a un corporativo con autorización para cargar a la cuenta',
+          'Una recogida donde el remitente nos cancela el flete, en el momento que recoge',
+          'Un dinero que el remitente nos debe de cancelar por concepto al valor del producto',
+          'Todas las anteriores son correctas',
+        ],
+        correct: 0,
+      },
+    ],
+  },
+
+  'Aliados GOO': {
+    questions: [
+      {
+        question: 'La EIE debemos colocarla Cuando?',
+        options: [
+          'Cuando Llegamos a la terminal',
+          'Cuando Visitamos el destinatario y se presenta una novedad',
+          'Cuando voy a entregar la unidad a NYS',
+          'Ninguna de las anteriores',
+        ],
+        correct: 1,
+      },
+      {
+        question: 'Cuál es el proceso de legalización para un aliado goo',
+        options: [
+          'Sigo reparto - Autorizar - Imprimir',
+          'Entregas II - CSR-Despachos',
+          'Sigo Legalización - Pendientes - Efectivas - Fallidas',
+          'Todas las anteriores',
+        ],
+        correct: 2,
+      },
+      {
+        question:
+          'El aliado goo cuando termine todas sus entregas y recogidas, se debe acercar a legalizar al punto droop más cercano de su casa',
+        options: ['Verdadero', 'Falso'],
+        correct: 1,
+      },
+      {
+        question:
+          'Cuando se entrega el chaleco a un aliado goo se debe firmar un documento o un acta.',
+        options: ['Verdadero', 'Falso'],
+        correct: 0,
+      },
+      {
+        question:
+          'Cuando a un aliado se le extravía la mercancía o es hurtado, se debe diligenciar un formato de pérdida y enviarlo a seguridad para su respectiva investigación.',
+        options: ['Verdadero', 'Falso'],
+        correct: 0,
+      },
+      {
+        question:
+          'Cuando un aliado trae novedades, Cuál es el Equipo estado que se le debe asignar',
+        options: [
+          '996 reexpediciones',
+          '997 tránsito',
+          '999 plataforma',
+          '993 aliados Independientes',
+          'Todas las anteriores',
+        ],
+        correct: 3,
+      },
+      {
+        question:
+          'Cuál es la Herramienta de consulta de un contralor goo para visualizar si un aliado está activo o no',
+        options: [
+          'Monitor Goo Reexpediciones',
+          'Monitor Goo Aliados',
+          'Stock Entregas Pendientes',
+          'Todas las anteriores',
+        ],
+        correct: 1,
+      },
+      {
+        question: 'Como se desactiva a un aliado.',
+        options: [
+          'Monitor Reexpediciones -Desactivar-Enviar Correos a DDHH',
+          'Monitor Aliados Goo - Enviar Correo a Coordinador de zona',
+          'Monitor Aliados Goo - Enviar Correo a DDHH',
+          'Desactivar-Motivo',
+          'A y C son correctas',
+          'C y D son correcta',
+        ],
+        correct: 5,
+      },
+      {
+        question: 'Quien le asigna las rutas a los aliados independientemente.',
+        options: [
+          'Coordinador de zona',
+          'Contralor Red aliados',
+          'Jefe de producción',
+          'Jefe operaciones Am',
+          'Gerente Regional',
+          'A,B y E son correctas',
+          'C,E y A son correctas',
+          'A,B y D son correctas',
+        ],
+        correct: 7,
+      },
+      {
+        question:
+          'Por qué medios se pueden legalizar los dineros recaudados durante el día.',
+        options: [
+          'Efectivo',
+          'Consignación al banco Davivienda',
+          'Consignación a Bancolombia',
+          'Consignación AV Villas',
+          'A y D son correctas',
+          'A y C son correctas',
+          'Todas las anteriores',
+        ],
+        correct: 4,
+      },
+      {
+        question:
+          'En el momento de legalizar a los aliados independientes estos deben quedar totalmente sin recogidas y entregas en el aplicativo, y mostrar el dispositivo al contralor Red aliado o el que haga sus veces.',
+        options: ['Verdadero', 'Falso '],
+        correct: 0,
+      },
+    ],
+  },
+
+  Reexpediciones: {
+    questions: [
+      {
+        question: '¿Cuál de las siguientes opciones son tipos de rutas?',
+        options: [
+          'Cuenta corriente, mercancía, Flete pago',
+          'Aprendizaje, mejoramiento, Diferenciadora',
+          'Local, doméstica, nacional, reexpedición',
+          'Ninguna de las anteriores',
+        ],
+        correct: 2,
+      },
+      {
+        question:
+          'Si la unidad viene con dirección de entrega de un Aliado Droop como se ingresa la unidad al sistema.',
+        options: [
+          'Asignar con checkpoint de asignación al equipo aliado',
+          'Aplicativo goo opción más traslado',
+          'Aplicativo goo recibir en punto Droop',
+          'Ninguna de las anteriores',
+        ],
+        correct: 2,
+      },
+      {
+        question:
+          'Cuantos días hábiles tiene el reexpedidor para legalizar las guías efectivas y los dineros.',
+        options: ['3 Días', '8 Días', '2 Días', '5 Días'],
+        correct: 3,
+      },
+      {
+        question:
+          'Las guías asignadas al reexpedidor por cual aplicativo se pueden visualizar.',
+        options: [
+          'Sigoo legalización',
+          'Sigoo reparto',
+          'Monitor Goo',
+          'Ninguna de las anteriores',
+        ],
+        correct: 2,
+      },
+      {
+        question:
+          'Al momento de dar fecha de entrega las guías de los reexpedidores los pasos a seguir son:',
+        options: [
+          'Entregas II - reexpedidores',
+          'Reexpedidores - Entregas II',
+          'Reexpedidores - Entregas - Legalización - Entregas II',
+          'Ninguna de las anteriores',
+        ],
+        correct: 2,
+      },
+      {
+        question:
+          'Donde puedo ver las guías próximas a vencer por cps y vencidas',
+        options: [
+          'Por Qlik Sense tablero de reexpediciones',
+          'Por intranet',
+          'Monitor goo y Reexpedidores 2.0',
+          'A y B son verdaderas',
+        ],
+        correct: 3,
+      },
+      {
+        question:
+          'Donde puedo visualizar el dinero total que nos debe el reexpedidor por guías que ya tuvieron conectividad por el reexpedidor',
+        options: [
+          'Por intranet reexpediciones pendientes',
+          'Por Qlik Sense',
+          'Monitor Goo',
+          'Ninguna de las anteriores',
+        ],
+        correct: 0,
+      },
+      {
+        question:
+          'Cuál es el proceso que seguir cuando el reexpedidor nos trae unidades fallidas',
+        options: [
+          'No se hace ningún procedimiento',
+          'Se asignan al estado 996 se legaliza como fallida en el programa de reexpedidores 2.0 posteriormente se envían a NYS',
+          'Se asignan al estado 999',
+          'Ninguna de las anteriores',
+        ],
+        correct: 1,
+      },
+      {
+        question:
+          'Si el reexpedidor tiene novedad de cartera a quien se le informa ',
+        options: [
+          'Gerencia',
+          'Jefe De producción',
+          'Coordinador Regional de Redes',
+          'Todas las anteriores',
+        ],
+        correct: 3,
+      },
+    ],
+  },
+
+  Servicios: {
+    questions: [
+      {
+        question:
+          'Las siglas que identifican los productos:¿Mercancía – ¿Radicación de documentos - ¿Firma de documentos – ¿Paquete, en su orden son?',
+        options: [
+          'CC – RD – CT – PAQ',
+          'FCE – MCIA – CM – FD',
+          'FD – PAQ – RD – MCIA',
+          'FD – PAQ – RD – MCIA',
+        ],
+        correct: 2,
+      },
+      {
+        question:
+          '¿Es la Ruta que se realiza entre terminales Coordinadora, Origen – Destino? ¿Se realiza con vehículos de alta capacidad como tracto camiones(mulas)?',
+        options: [
+          'Ruta de reexpedición',
+          'Ruta Nacional',
+          'Población directa',
+          'Ruta Domestica',
+        ],
+        correct: 1,
+      },
+      {
+        question: '¿Cuáles son las funciones del empaque?',
+        options: [
+          'Proteger, contener',
+          'Conservar y facilitar la distribución',
+          'A Y B son correctas',
+          'Ninguna de las anteriores',
+        ],
+        correct: 2,
+      },
+      {
+        question: '¿Unidades que se mueven por fuera de las bandas?',
+        options: [
+          'Exhibidores, varillas, bultos',
+          'Cajas y baúles',
+          'Persianas, autopartes',
+          'A y C son correctas',
+        ],
+        correct: 3,
+      },
+      {
+        question:
+          'Son algunas de las condiciones para realizar de forma correcta un arrume en un vehículo de ruta nacional:',
+        options: [
+          'Iniciar el arrume de piso a techo y de pared a pared',
+          'Las unidades de empaque deben entrelazar como “ladrillos” para generar resistencia en el arrume y evitar su movimiento en el transporte.',
+          'Los huacales, cuñetes y canecas por su peso y la resistencia del empaque son base de arrume',
+          'Todas las anteriores',
+        ],
+        correct: 3,
+      },
+      {
+        question: '¿Qué es un Rotulo? “Única respuesta”',
+        options: [
+          'Es la identificación de la unidad y lo realizan principalmente los clientes de FP y FCE. Contiene los datos básicos del envío',
+          'Conservar y facilitar la distribución',
+          'No es necesario en la logística',
+          'Ninguna de las anteriores',
+        ],
+        correct: 0,
+      },
+      {
+        question:
+          '¿Los clientes corporativos y de contado tienen los siguientes fletes?',
+        options: [
+          'RD -FD',
+          'FP - FCE – CC – AS',
+          'RCE – CT',
+          'Ninguna de las anteriores',
+        ],
+        correct: 3,
+      },
+      {
+        question:
+          '¿Es el proceso de lectura dentro del ciclo del servicio realizado a la etiqueta inteligente o a un código dummy asociado, para garantizar el control de ubicación de la unidad?',
+        options: ['Toque', 'Clasificación', 'Control ', 'Checkpoint'],
+        correct: 3,
+      },
+    ],
+  },
+  // Terminales: {},
+};
